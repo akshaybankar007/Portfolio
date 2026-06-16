02df5ca4d7bd6b55bebcbb7e976adcb7eb5f7fe0
akshaybankar007 <akshaybankar076@gmail.com>
2026-06-16 06:07:33 +0530

    refactor: clean up layout components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,12 +15,12 @@ export default function App() {
       <Navbar />
       <main>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/experience" element={<Experience />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/skills" element={<Skills />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route path="/" element= { <Home />} />
+          <Route path="/about" element= { <About />} />
+          <Route path="/experience" element= { <Experience />} />
+          <Route path="/projects" element= { <Projects />} />
+          <Route path="/skills" element= { <Skills />} />
+          <Route path= " /contact" element = { <Contact/>} />
         </Routes>
       </main>
       <Footer />
